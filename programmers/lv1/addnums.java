8201762d05fe45c76e86b2d334210ccea11110c6
Seoha-Yoon <seohayn127@gmail.com>
2022-01-03 16:54:45 +0900

    test

---
+++ programmers/lv1/addnums.java
@@ -0,0 +1,30 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2580
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;\f1\fnil\fcharset129 AppleSDGothicNeo-Regular;}
+{\colortbl;\red255\green255\blue255;}
+{\*\expandedcolortbl;;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\pard\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803\pardirnatural\partightenfactor0
+
+\f0\fs24 \cf0 import java.util.*;\
+\
+class Solution \{\
+    public int solution(int[] numbers) \{\
+        // 1
+\f1 \'ba\'ce\'c5\'cd
+\f0  9
+\f1 \'b1\'ee\'c1\'f6
+\f0  
+\f1 \'b4\'d9
+\f0  
+\f1 \'b4\'f5\'c7\'d1
+\f0  
+\f1 \'b0\'aa
+\f0 \
+        int answer = 45;\
+        \
+        for(int n: numbers)\
+            answer -=n;\
+        \
+        return answer;\
+    \}\
+\}}
