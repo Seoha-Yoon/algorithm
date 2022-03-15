74e63adb6e64b587b8fe7c0ca52931c4deb42055
Seoha-Yoon <seohayn127@gmail.com>
2022-03-15 11:38:49 +0900

    런타임에러 해결

--- boj/src/CLASS3/boj11727.java
+++ boj/src/CLASS3/boj11727.java
@@ -8,12 +8,19 @@ public class boj11727 {
         int N = sc.nextInt();
         int[] arr = new int[N+1];
 
-        arr[1] = 1;
-        arr[2] = 3;
-        for (int i = 3; i <= N; i++) {
-            arr[i] = (arr[i-1]+arr[i-2]*2)%10007;
+        if(N==1) {
+            System.out.println(1);
+            return;
+        }else if(N==2){
+            System.out.println(3);
+            return;
+        }else{
+            arr[1] = 1;
+            arr[2] = 3;
+            for (int i = 3; i <= N; i++) {
+                arr[i] = (arr[i-1]+arr[i-2]*2)%10007;
+            }
         }
-
         System.out.println(arr[N]);
     }
 }
